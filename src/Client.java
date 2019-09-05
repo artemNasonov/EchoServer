3b3ecf65f0868c9f071f7e1b12e70409c57bee45
Artemiin <arteom.nasonov@yandex.ru>
2019-09-06 00:45:24 +0300

    Multy-Thread Chat

--- src/Client.java
+++ src/Client.java
@@ -1,5 +1,6 @@
 import java.io.*;
 import java.net.Socket;
+import java.util.Scanner;
 
 public class Client {
     public static void main(String[] args) {
@@ -9,16 +10,39 @@ public class Client {
     Client(){
         try {
             Socket socket = new Socket("127.0.0.1", 54321);
-            BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-            BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-            PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
-            System.out.println("Opened threads for read and write. Now you can write message here:");
-            while(true){
-                out.write(reader.readLine() + "\n");
-                out.flush();
-                String answer = in.readLine();
-                System.out.println(answer);
-            }
+            Runnable sendMessage = () -> {
+                Scanner s = new Scanner(new InputStreamReader(System.in));
+                PrintWriter out = null;
+                try {
+                    out = new PrintWriter(socket.getOutputStream(), true);
+                    System.out.println("Please, enter your nickname:");
+                    out.write(s.nextLine() + "\n");
+                    out.flush();
+                    while (true){
+                        if(s.hasNextLine()){
+                            out.write(s.nextLine()+"\n");
+                            out.flush();
+                        }
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            };
+            Runnable reciveMessage = () -> {
+                Scanner in = null;
+                try {
+                    in = new Scanner(new InputStreamReader(socket.getInputStream()));
+                    while(true){
+                        if(in.hasNextLine()) System.out.println(in.nextLine());
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            };
+            Thread outThread = new Thread(sendMessage);
+            Thread inThread = new Thread(reciveMessage);
+            inThread.start();
+            outThread.start();
         } catch (IOException e) {
             e.printStackTrace();
         }
--- src/Server.java
+++ src/Server.java
@@ -10,25 +10,35 @@ public class Server {
 
     private Server(){
         try {
-            ArrayList<Thread> allClients = new ArrayList<>();
+            ArrayList<Thread> allThreads = new ArrayList<>();
+            ArrayList<Socket> allSockets = new ArrayList<>();
             ServerSocket serverSocket = new ServerSocket(54321);
             System.out.println("Server started on port 54321");
             while(true){
                 Socket sock = serverSocket.accept();
                 Runnable r = () -> {
+                    String nickname = "";
                     try{
                         BufferedReader in = new BufferedReader(new InputStreamReader(sock.getInputStream()));
                         PrintWriter out = new PrintWriter(sock.getOutputStream(), true);
+                        nickname=in.readLine();
+                        out.write("It's server. Hello, " + nickname +", now you can chat with others: \n");
+                        out.flush();
                         while(true){
-                            out.write("It's server. You wrote: " + in.readLine() + "\n");
-                            out.flush();
+                            String message = in.readLine();
+                            for(Socket s: allSockets){
+                                PrintWriter o = new PrintWriter(s.getOutputStream());
+                                o.write(nickname + " wrote: " + message + "\n");
+                                o.flush();
+                            }
                         }
                     } catch (IOException e) {
-                        e.printStackTrace();
+                        System.out.println(nickname + " has disconnected");
                     }
                 };
-                allClients.add(new Thread(r));
-                allClients.get(allClients.size()-1).start();
+                allThreads.add(new Thread(r));
+                allThreads.get(allThreads.size()-1).start();
+                allSockets.add(sock);
             }
         } catch (IOException e) {
             e.printStackTrace();
